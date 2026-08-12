4e542309cd7276bb220651f629403061cfb224b1
Ivan Podgurskiy <ivan.podgurskiy@gmail.com>
2026-08-12 16:20:50 +0300

    ci: isolate quality tools from minimum lane

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule SpdxExpression.MixProject do
       {:stream_data, "~> 1.1", only: [:dev, :test]},
       {:jason, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: :dev, runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
     ]
   end
